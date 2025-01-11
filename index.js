e7089f9b870d7b17a7183d6ec7aac6c6fcf968bd
Krzysztof Kozlowski <kozlofski.dev@gmail.com>
2025-01-11 17:27:34 +0100

    Basic loading of data to console

---
+++ index.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => console.log("loaded"));
+
+const BASE_URL = "https://rickandmortyapi.com/api/character";
+
+async function loadAllCharacters() {
+  try {
+    const response = await fetch(BASE_URL);
+    const json = await response.json();
+
+    const body = document.querySelector("body");
+    console.dir(json);
+  } catch (error) {}
+}
+
+loadAllCharacters();
